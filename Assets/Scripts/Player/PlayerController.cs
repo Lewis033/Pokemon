7f07d8ad8d137412608f79e658c616e8684910b6
arthur-buyens <arthur.buyens@gmail.com>
2026-09-24 23:12:58 -0400

    Ajout des hautes herbes et logique de rencontre Pokémon sauvage

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     public float moveSpeed;
     public LayerMask solidObjectsLayer;
+    public LayerMask grassLayer;
 
     private bool isMoving;
     private Vector2 input;
@@ -67,6 +68,8 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+
+        CheckForEncounters();
     }
 
     private bool IsWalkable(Vector3 targetPos)
@@ -77,5 +80,16 @@ public class PlayerController : MonoBehaviour
         }
 
         return true;
+    }
+
+    private void CheckForEncounters()
+    {
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, grassLayer) != null)
+        {
+            if (Random.Range(1, 101) <= 10)
+            {
+                Debug.Log("Vous avez rencontré un Pokémon sauvage");
+            }
+        }
     }
 }
